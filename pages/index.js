ab83e63a96902de6d909d7d99b3535d36b480c96
Liran Cohen <liran@bookaway.com>
2019-07-23 14:37:12 +0300

    init commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import App from '../components/App'
+import Header from '../components/Header'
+import Submit from '../components/Submit'
+import PostList from '../components/PostList'
+
+export const config = { amp: true }
+
+
+export default () => {
+  console.log('render!!')
+  return (
+    <div> hello world</div>
+
+  )
+}
